be1f4445aa49dd843d72553ea3e160c5d043a62c
Lamøy <eirik.lamoy0@nav.no>
2023-09-19 11:34:53 +0200

    Seminar mm.

--- 2023-09-09 seimar_3.R
+++ 2023-09-09 seimar_3.R
@@ -45,23 +45,44 @@ df_sim2 <- tibble(
 )
 
 
-# 4 
+# 4 Show how the approx work over time:
 df_sim2_long <- df_sim2 |> 
     pivot_longer(-n_sim) |> 
     unnest( value) |> 
     #filter( n  >  1) |> 
     mutate( value = value - pi )
 
-
+# How the diff. approx work:
 df_sim2_long |> 
     mutate( name = factor(name)) |> 
     ggplot( aes( y = value, x = n_sim, color = name) ) + 
     geom_line( ) +
     ylim( c(-0.01, 0.01))
 
+# Importing and cleaning data ---------------------------------------------
+
+df <- vroom::vroom("data/2019-09-11_partifordeling_1_fy_2019.csv")
 
 
+votes <- df |> 
+    # 1) Filter
+    filter( Fylkenavn != "Oslo") |> 
+    # 2) Vites
+    mutate( votes = `Antall stemmer totalt`) |> 
+    # 3) select
+    select( Fylkenavn, Fylkenummer, Partikode, votes) |> 
+    # 4) Change som names
+    mutate( 
+        across( .cols = c("Fylkenavn", "Partikode"),
+                .fns = \(x) str_replace_all(x, "\\?\\?", "O")
+                )
+            ) |> 
+    mutate( Fylkenavn = str_to_sentence(Fylkenavn))
 
-
+# 4) 
+votes |> 
+    filter( Partikode %in% c("A", "SV", "RODT", "SP", "FRP", "KRF", "MDG", "H", "FNB", "V")
+            ) |> 
+    pivot_wider( names_from = Partikode, values_from = votes)
 
 
